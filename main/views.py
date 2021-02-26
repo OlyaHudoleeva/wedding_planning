10f41d0b770ef1b944512e250761864031a91653
Volha Khudaleyeva <olga.hudoleeva98@gmail.com>
2021-02-26 18:19:45 +0300

    Добавлена главная страница

--- main/views.py
+++ main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h4>Main page</h4>")
+    return render(request, 'main/index.html')
 
 def guests(request):
-    return HttpResponse("<h4>Guests list</h4>")
+    return render(request, 'main/guests.html')
